ddd9af5e41af3c136300c984f9bd499a4bf9efac
27man99 <84024550+27man99@users.noreply.github.com>
2021-05-12 18:07:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! This is my NEW WEB APP from EST ..."
+    return "Hello, World! This is my NEW WEB APP from EST ...."
